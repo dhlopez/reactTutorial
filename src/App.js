ebd78379791a3db173a08ca7c45a8523fe17865b
David Lopez <dhelaman@hotmail.com>
2017-05-07 00:42:15 -0400

    server and client codes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,61 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class CodeServer extends React.Component
+{
+  render()
+  {
+    return(
+      <div class="codeServer">
+      <h4>{this.props.code} - {this.props.status}</h4>
+      </div>
+    );
+  }
+}
+class CodeClient extends React.Component
+{
+  render()
+  {
+    return(
+      <div class="codeClient">
+        <h4>{this.props.code}</h4>
+        <button id={this.props.code}>Use</button>
+      </div>
+    );
+  }
+}
+
+class ServerPanel extends React.Component
+{
+  getServerCode(code, status)
+  {
+    return <CodeServer code={code} status={status}/>
+  }
+  render()
+  {
+    return(
+      <div class="client">
+        {this.getServerCode("abc", "valid")}
+      </div>
+    );
+  }  
+}
+class ClientPanel extends React.Component
+{
+  getClientCode(code)
+  {
+    return <CodeClient code={code}/>
+  }
+  render()
+  {
+    return(
+      <div class="server">
+        {this.getClientCode("abc")}
+      </div>
+    );
+  }  
+}
+
 function Welcome(props)
 {
   return <h1>Hello, {props.name}</h1>;
@@ -11,7 +66,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Welcome name="Dave" />
+        <Welcome name="Dave" />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -19,6 +74,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ServerPanel/>
+        <ClientPanel/>
       </div>
     );
   }
